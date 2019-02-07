52b67266fb751e1e84f521064840af518d24d829
Noel Barrios <noel.barrios@optum.com>
2019-02-07 08:00:21 -0600

    data model in Notes.cs; define UI for new page to begin a multi-page app

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        // Auto-implemented properties for trivial get and set
+        public string FileName { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
+
+
+// this class defines a Note model that will store data about each note in the application.  
+
+
+// ABOUT MVC: 
+// In the MVC pattern, the model represents the data, and does nothing else.
+// The model does NOT depend on the controller or the view.  
+// The view displays the model data, and sends user actions (e.g. button clicks) to the controller.
---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
+
