c1a1a2ebc6b4f8bd672ab7534a0fd657777e7e74
Noel Barrios <noel.barrios@optum.com>
2019-02-07 11:05:23 -0600

    crete code-behind for NotesPage

--- Notes/NotesPage.xaml.cs
+++ Notes/NotesPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Linq;
+using Notes.Models;
 using Xamarin.Forms;
 
 namespace Notes
@@ -11,5 +13,51 @@ namespace Notes
         {
             InitializeComponent();
         }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+
+            // Directory.Enumerate returns an enumerable collection of file names that match a search pattern in the specified path. 
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach(var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+
+            listView.ItemsSource = notes.OrderBy(d => d.Date).ToList();
+        }
+
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+
+        }
+
+
+
     }
 }
