921a45ed9ff645c37a2e971daeeb0693df05f415
Noel Barrios <noel.barrios@optum.com>
2019-02-07 08:57:22 -0600

    create UI for NotesPage

---
+++ Notes/NotesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
